09cd3bddf72d709945f0f44688023efed1b2e83d
sasuke5055 <maciek.lewandowicz@gmail.com>
2019-11-16 16:40:36 +0100

    cd listy 3

--- DB L3/Lista3.sql
+++ DB L3/Lista3.sql
@@ -45,3 +45,174 @@ SELECT imie, dataUrodzenia FROM osoba JOIN zwierzak ON osoba.id=zwierzak.ID ORDE
 EXPLAIN SELECT imie, dataUrodzenia FROM osoba JOIN zwierzak ON osoba.id=zwierzak.ID ORDER BY osoba.dataUrodzenia ASC LIMIT 1;
 
 
+#zad3
+
+CREATE TABLE zawody
+(
+id int NOT NULL AUTO_INCREMENT,
+nazwa varchar(40),
+pensja_min int,
+pensja_max int,
+CHECK (pensja_min>=2100),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE praca
+(
+idPracownika int NOT NULL,
+idZawodu int NOT NULL,
+pensja int
+);
+
+CREATE TABLE nZawodow
+(
+id int NOT NULL AUTO_INCREMENT,
+nazwa varchar(40),
+PRIMARY KEY (id)
+);
+
+INSERT INTO nZawodow (nazwa) VALUES ('Papiez'),('Pisarz'),('Nauczyciel'),('Informatyk'),('Prawnik'),('Kierowca'),('Lekarz'),('Sprzedawca'),('Konduktor'),('Strazak'),('Policjant');
+
+SET @counter =1;
+
+DELIMITER $$
+CREATE PROCEDURE addZawody ()
+BEGIN 
+SET @counter =1;
+WHILE @counter<12 DO  
+	INSERT INTO zawody (nazwa,pensja_min,pensja_max) VALUES ( (SELECT nazwa FROM nZawodow WHERE id=@counter),2100+ FLOOR(RAND()*1000),3100+ FLOOR(RAND()*6000) );
+    SET @counter=@counter+1;
+	
+    END WHILE;
+END;$$
+DELIMITER ;
+CALL addZawody();
+
+SELECT * FROM zawody;
+DROP PROCEDURE addZawody;
+DROP TABLE nZawodow;
+
+
+DELIMITER $$
+CREATE PROCEDURE kursor ()
+BEGIN
+	
+    DECLARE koniec, j SMALLINT UNSIGNED;
+    DECLARE idP int;
+    DECLARE idZ int;
+    DECLARE pensja int;
+
+	DECLARE kur CURSOR FOR SELECT id FROM osoba;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET koniec = 1;
+    
+    OPEN kur;
+    petla: LOOP
+		FETCH kur INTO idP;
+        
+        IF koniec = 1 THEN 
+			LEAVE petla;
+		END IF;
+        
+        SET idZ = CEIL(RAND()*11);
+        SET pensja = FLOOR(RAND()*(SELECT pensja_max-pensja_min FROM zawody WHERE id=idZ))+(SELECT pensja_min FROM zawody WHERE id=idZ);
+        INSERT INTO praca VALUES (idP,idZ,pensja);
+    END LOOP petla;
+END; $$
+DELIMITER ;
+
+DROP PROCEDURE kursor;
+
+CALL kursor();    
+    
+#zad4
+
+    
+DELIMITER $$
+CREATE PROCEDURE zad4 (
+IN agg VARCHAR(20), 
+IN kol VARCHAR(20), 
+OUT X TEXT)
+
+BEGIN
+    SET X = NULL;
+    SET @result='';
+	
+    IF LOWER(kol) = 'imie' OR
+		LOWER(kol) = 'plec' OR
+		LOWER(kol) = 'nazwisko' OR
+		LOWER(kol) = 'dataUrodzenia' THEN
+        SET @result='';
+	ELSE
+		SIGNAL SQLSTATE '45000' 
+    SET MESSAGE_TEXT = 'Niepoprawna nazwa kolumny';
+	END IF;
+    
+    
+    IF UPPER(agg) = 'COUNT' AND LOWER(kol) IN ('imie','plec') THEN
+		SET @question = CONCAT('SELECT COUNT(',kol,') FROM osoba INTO @result');
+		PREPARE statement FROM @question;
+		EXECUTE statement;
+		DEALLOCATE PREPARE statement;
+        
+	ELSEIF UPPER(agg) = 'GROUP_CONCAT' AND LOWER(kol) IN ('imie','nazwisko','dataUrodzenia','plec') THEN
+		SET @question = CONCAT ('SELECT group_concat(',kol,') FROM osoba INTO @result');
+		PREPARE statement FROM @question;
+		EXECUTE statement;
+		DEALLOCATE PREPARE statement;
+
+	ELSEIF UPPER(agg) IN ('MIN','MAX') AND LOWER(kol) IN ('dataUrodzenia') THEN
+		SET @question = CONCAT ('SELECT ',agg,'(dataUrodzenia) FROM osoba INTO @result');
+		PREPARE statement FROM @question;
+		EXECUTE statement;
+		DEALLOCATE PREPARE statement;
+        
+	ELSEIF UPPER(agg) IN ('AVG','STD','VAR_POP') AND LOWER(kol) IN ('dataUrodzenia') THEN
+		SET @question = CONCAT ('SELECT ',agg,'(YEAR(CURDATE())-YEAR(dataUrodzenia)) FROM osoba INTO @result');
+		PREPARE statement FROM @question;
+		EXECUTE statement;
+		DEALLOCATE PREPARE statement;
+	
+    ELSE
+		SIGNAL SQLSTATE '45000' 
+		SET MESSAGE_TEXT = 'Niepoprawna nazwa funkcji agregującej lub próba wykonania jej na złych kolumnach';
+	
+    END IF;
+    
+SET X = CONCAT(kol,", ",agg,", ",@result)  ;  
+
+END; $$
+DELIMITER ;
+
+
+CALL zad4('count','imie',@wynik);
+SELECT @wynik;
+
+CALL zad4('count','nazwisko',@wynik);
+SELECT @wynik;
+
+CALL zad4('GROUP_CONCAT','plec',@wynik);
+SELECT @wynik;
+
+CALL zad4('Max','dataUrodzenia',@wynik);
+SELECT @wynik;
+
+CALL zad4('std','dataUrodzenia',@wynik);
+SELECT @wynik;
+    
+
+DROP PROCEDURE zad4;
+
+#zad5
+
+
+
+
+
+
+
+
+
+
+
+
